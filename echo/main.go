7989d3defe8426173e7c4d687bac81a1f4ea97f3
ehe@jubiimediagroup.dk <ehe@jubiimediagroup.dk>
2019-10-14 12:04:24 +0200

    Syntax fix

--- echo/main.go
+++ echo/main.go
@@ -1,20 +1,19 @@
-
 package main
 
 import (
-    "fmt"
-    "github.com/aws/aws-lambda-go/events"
-    "github.com/aws/aws-lambda-go/lambda"
-)
+	"fmt"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Println("Received body: ", request.Body)
-	return events.APIGatewayProxyResponse{Body: request.Body, Headers: {"Access-Control-Allow-Origin": "*"} StatusCode: 200}, nil
+	return events.APIGatewayProxyResponse{Body: request.Body, Headers: {"Access-Control-Allow-Origin": "*"}, StatusCode: 200}, nil
 }
 
 func factorial(number int) int {
-	if(number == 0){
+	if number == 0 {
 		return 0
 	}
 	return (number * factorial(number-1))
